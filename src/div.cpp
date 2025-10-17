4094b73c6248eaaf7d88d0e1bcc4e66015206465
Lázaro Gabriel <114248943+lazarobo@users.noreply.github.com>
2025-10-17 17:14:03 +0000

    initial

---
+++ src/div.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int divide(int numerador, int denominador) {
+    int q, r;
+
+    
+
+    return q;
+}
+
+int main() {
+    int a, b;
+    int resultado;
+
+    if (resultado = divide(a, b)) {
+        std::cout << a << " / " << b << " = " << resultado << '\n';
+    } else {
+        std::cout << "Erro: divisão por zero\n";
+    }
+
+    return 0;
+}
